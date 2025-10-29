a7c4d26aeb0723c7359572f0a6cff042e6488cab
Bandaru Mercy <bandarumercy11@gmail.com>
2025-10-29 18:11:03 +0530

    Added a class named Mercy.cs

---
+++ Git_Collaboration_Demo_Console/Mercy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Collaboration_Demo_Console
+{
+    internal class Mercy
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
